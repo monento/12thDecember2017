cde355210e9f28d621c1220bc5fa41c1bddcfc09
nmakarychev <nmakarychev@mail.ru>
2017-12-12 22:21:18 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/Задача 1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static int InputInt()
+        {
+            bool ok;
+            int Num;
+            do
+                ok = Int32.TryParse(Console.ReadLine(), out Num);
+            while (!ok);
+            return Num;
+        }
+        static int[,] FormArray(int N, int M)
+        {
+            int [,] A = new int [N,M];
+            Random rnd = new Random();
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < M; j++)
+                    A[i, j] = rnd.Next(0, 10);
+            return A;
+        }
+        static void PrintArr(int[,] A)
+        {            
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                for (int j = 0; j < A.GetLength(1); j++)
+                    Console.Write(" " + A[i, j]);
+                Console.WriteLine();
+            }
+        }
+        static int InputK(int N)
+        {
+            int K; bool ok;
+            do
+            {
+                K = InputInt();
+                if ((K > 0) && (K <= N+1)) ok = true;
+                else ok = false;
+            }
+            while (!ok);
+            return K;
+        }
+        static int[,] AddStr(int[,] A, int N, int M, int K)
+        {
+            Random rnd = new Random();
+            int[,] T = new int[N + 1, M];
+            for (int i = 0; i < N + 1; i++)
+               for (int j = 0; j < M; j++)
+                {
+                    if (i < K - 1) T[i, j] = A[i, j];
+                    if (i == K - 1) T[i, j] = rnd.Next(-9, 0);
+                    if (i > K - 1) T[i, j] = A[i - 1, j];
+                }
+            A = T;
+            return A;   
+        }
+        static void Main(string[] args)
+        {
+            int N, M;
+            Console.WriteLine("Введите число строк");               
+            N=InputInt();
+            Console.WriteLine("Введите число столбцов");
+            M = InputInt();
+            Console.WriteLine("Начальный массив");
+            int[,] A = FormArray(N, M);
+            PrintArr(A);
+            Console.WriteLine("Введите число K");
+            int K=InputK(N);
+            Console.WriteLine("Конечный массив");
+            A = AddStr(A, N, M, K);
+            PrintArr(A);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Задача 2/Задача 2.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_2
+{
+    class Program
+    {
+        static int IntputInt()
+        {
+            bool ok;
+            int Num;
+            do
+            {
+                ok = Int32.TryParse(Console.ReadLine(), out Num);
+            }
+            while (!ok);
+            return Num;        
+        }
+        static int [][] FormArr(int[][] A)
+        {
+            Random rnd = new Random();
+            int K;
+            for (int i = 0; i < A.Length; i++)
+            {
+                K = rnd.Next(1, 10);
+                A[i] = new int[K];
+                for (int j = 0; j < K; j++)
+                {
+                    A[i][j] = rnd.Next(0, 10);
+                }
+            }
+            return A;
+        }
+        static void PrintArr(int[][] A)
+        {
+            for (int i = 0; i < A.Length; i++)
+            { 
+                for (int j = 0; j < A[i].Length; j++)
+                    Console.Write(" " + A[i][j]);
+                Console.WriteLine();
+            }            
+        }
+        static int[][] DeleteStrg(int [] []A)
+        {
+            int min = Int32.MaxValue; 
+            int h=1;
+            for (int i = 0; i < A.Length; i++)                  //выявление минимальной длины;
+            {
+                if (A[i].Length < min) min = A[i].Length;
+            }
+            for (int j = 0; j < A.Length; j++)                //Выявление номера строки;
+                if(A[j].Length==min) { h = j;  break; }
+
+            int[][] T = new int [A.Length - 1][];
+
+            for (int i = 0; i < T.Length; i++)                  //Удаление строки
+            {
+                if (i >= h) T[i] = A[i+1];
+                else if (i<h) T[i] = A[i];
+            }            
+            return T;
+            
+        }        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввидите число строк N");
+            int N = IntputInt();
+            Console.WriteLine("Начальный массив");
+            int[][] A = new int[N][];
+            FormArr(A);
+            PrintArr(A);
+            Console.WriteLine("Конечный массив");          
+            A=DeleteStrg(A);
+            PrintArr(A);
+            Console.ReadLine();
+        }
+    }
+}
